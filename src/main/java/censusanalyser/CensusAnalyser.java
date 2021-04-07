877fdd3625b06c9823dfbabc704d6e760ac88fe0
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-07 19:10:57 +0530

    Refactored: added Invalid extension type in exception class and created method to find file exetension and verfi while performing operations

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -10,23 +10,42 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class CensusAnalyser {
-    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+    public int loadIndiaCensusData(String filePath) throws CensusAnalyserException {
+        String extension = findExtenstionTypeOfFile(filePath);
+        int namOfEateries = 0;
         try {
-            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(IndiaCensusCSV.class);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
-            int namOfEateries = 0;
-            while (censusCSVIterator.hasNext()) {
-                namOfEateries++;
-                IndiaCensusCSV censusData = censusCSVIterator.next();
+            if (extension.equalsIgnoreCase("csv")) {
+                Reader reader = Files.newBufferedReader(Paths.get(filePath));
+                CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+                csvToBeanBuilder.withType(IndiaCensusCSV.class);
+                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+                CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+                Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+
+                   while (censusCSVIterator.hasNext()) {
+                        namOfEateries++;
+                        IndiaCensusCSV censusData = censusCSVIterator.next();
+                    }
+            }
+            else {
+                throw new CensusAnalyserException("Invalid Extension type of file",CensusAnalyserException
+                                                                                    .ExceptionType
+                                                                                    .INVALID_FILE_EXTENSION);
             }
             return namOfEateries;
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
+    }
+
+    public String findExtenstionTypeOfFile(String pathValue) {
+        int index = pathValue.lastIndexOf('.');
+        String extension = null;
+        if( index > 0) {
+            extension = pathValue.substring(index + 1);
+            System.out.println("File extension is " + extension);
+        }
+        return extension;
     }
 }
