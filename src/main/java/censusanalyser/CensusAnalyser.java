9668ebe5d41374e8321110b5ba51ade4632e460c
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-12 14:09:04 +0530

    Refactored: code to use csv builder using external jar and separated it from analyser

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -1,10 +1,15 @@
 package censusanalyser;
+import com.bridgelabz.CSVBuilderException;
+import com.bridgelabz.CSVBuilderFactory;
+import com.bridgelabz.ICSVBuilder;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.stream.StreamSupport;
+
 
 public class CensusAnalyser {
     public int loadIndiaCensusData(String filePath) throws CensusAnalyserException {
